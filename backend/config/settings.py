a6633f4040d83185b275645ff8b37f5fa417c9c3
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-07-29 15:28:00 +0530

    UPDATE: include whisperX config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,3 +6,7 @@ load_dotenv()
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 BASE_MODEL_URL = "https://api-inference.huggingface.co/models/"
 MODEL_NAME = "facebook/mbart-large-50-many-to-many-mmt"
+
+WHISPER_MODEL_SIZE = "base"
+WHISPER_COMPUTE_TYPE = "float16"
+WHISPER_LANGUAGE_CODE = "en"
